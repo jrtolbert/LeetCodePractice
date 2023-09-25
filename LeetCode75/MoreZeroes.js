dc3a40557ce1e2e5f57b15192e6287beda34099b
James Tolbert <jrtolbert97@gmail.com>
2023-09-25 18:19:46 -0500

    Move Zeroes LeetCode75 solution

---
+++ LeetCode75/MoreZeroes.js
@@ -0,0 +1,25 @@
+
+var moreZeroes = (nums) => {
+    var containZeroes = []
+    while (true) {
+        var index = nums.indexOf(0)
+        if (index > -1) {
+            containZeroes.push(nums[index])
+            nums.splice(index, 1)
+        } else { 
+            break
+        }
+    }
+
+    while (containZeroes.length > 0) {
+        nums.push(containZeroes.pop())
+    }
+
+    console.log(nums)
+    console.log(containZeroes)
+}
+
+var number = [0]
+
+moreZeroes(number)
+console.log(number)
